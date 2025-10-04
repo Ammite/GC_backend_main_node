e241cc96a7fc82dea5e990763508258cb32ebb64
Амиржан <@gngtohell>
2025-10-04 11:34:47 +0500

    Добавил новые таблицы

---
+++ models/categories.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database.database import Base
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    iiko_id = Column(String(50), unique=True, nullable=False)
+    name = Column(String(255), nullable=False)
+
+    parent_id = Column(Integer, ForeignKey("Category.id"), nullable=True)
+    parent = relationship("Category", remote_side=[id], backref="children")
+
+
+    items = relationship("Item", back_populates="Category")
